23de7101529bb0053b3cb1327ce2aa27beba23cf
The Oddball <oddball@oddball.tf>
2019-07-22 15:38:03 -0700

    Major changes to autoinstaller
    Add 18.04 and 19.04, change off of beta .NET version, and format the files.

--- nadekoautoinstaller.sh
+++ nadekoautoinstaller.sh
@@ -1,46 +1,46 @@
 #!/bin/bash -e
 
-function detect_OS_ARCH_VER_BITS {
+function detect_OS_ARCH_VER_BITS() {
 	ARCH=$(uname -m | sed 's/x86_//;s/i[3-6]86/32/')
 	if [ -f /etc/lsb-release ]; then
-	    . /etc/lsb-release
-	    OS=$DISTRIB_ID
-	    VER=$DISTRIB_RELEASE
+		. /etc/lsb-release
+		OS=$DISTRIB_ID
+		VER=$DISTRIB_RELEASE
 	elif [ -f /etc/debian_version ]; then
-	    OS=Debian  # XXX or Ubuntu??
-	    VER=$(cat /etc/debian_version)
-	    SVER=$( cat /etc/debian_version | grep -oP "[0-9]+" | head -1 )
+		OS=Debian # XXX or Ubuntu??
+		VER=$(cat /etc/debian_version)
+		SVER=$(cat /etc/debian_version | grep -oP "[0-9]+" | head -1)
 	elif [ -f /etc/centos-release ]; then
 		OS=CentOS
-		VER=$( cat /etc/centos-release | grep -oP "[0-9]+" | head -1 )
+		VER=$(cat /etc/centos-release | grep -oP "[0-9]+" | head -1)
 	else
-	    OS=$(uname -s)
-	    VER=$(uname -r)
+		OS=$(uname -s)
+		VER=$(uname -r)
 	fi
 	case $(uname -m) in
 	x86_64)
-	    BITS=64
-	    ;;
+		BITS=64
+		;;
 	i*86)
-	    BITS=32
-	    ;;
+		BITS=32
+		;;
 	armv*)
-	    BITS=32
-	    ;;
+		BITS=32
+		;;
 	*)
-	    BITS=?
-	    ;;
+		BITS=?
+		;;
 	esac
 	case $(uname -m) in
 	x86_64)
-	    ARCH=x64  # or AMD64 or Intel64 or whatever
-	    ;;
+		ARCH=x64 # or AMD64 or Intel64 or whatever
+		;;
 	i*86)
-	    ARCH=x86  # or IA32 or Intel32 or whatever
-	    ;;
+		ARCH=x86 # or IA32 or Intel32 or whatever
+		;;
 	*)
-	    # leave ARCH as-is
-	    ;;
+		# leave ARCH as-is
+		;;
 	esac
 }
 
@@ -69,6 +69,10 @@ if [ "$OS" = "Ubuntu" ]; then
 		supported=1
 	elif [ "$VER" = "17.04" ]; then
 		supported=1
+	elif [ "$VER" = "18.04" ]; then
+		supported=1
+	elif [ "$VER" = "19.04" ]; then
+		supported=1
 	else
 		supported=0
 	fi
@@ -90,87 +94,116 @@ fi
 echo -e "Welcome to NadekoBot Auto Prerequisites Installer. \nWould you like to continue? \nYour OS: $OS \nOS Version: $VER \nArchitecture: $ARCH"
 
 while true; do
-    read -p "[y/n]: " yn
-    case $yn in
-        [Yy]* ) clear; echo Running NadekoBot Auto-Installer; sleep 2; break;;
-        [Nn]* ) echo Quitting...; rm nadekoautoinstaller.sh && exit;;
-        * ) echo "Couldn't get that please type [y] for Yes or [n] for No.";;
-    esac
+	read -p "[y/n]: " yn
+	case $yn in
+	[Yy]*)
+		clear
+		echo Running NadekoBot Auto-Installer
+		sleep 2
+		break
+		;;
+	[Nn]*)
+		echo Quitting...
+		rm nadekoautoinstaller.sh && exit
+		;;
+	*) echo "Couldn't get that please type [y] for Yes or [n] for No." ;;
+	esac
 done
 
 if [ "$OS" = "Ubuntu" ]; then
-echo "This installer will download all of the required packages for NadekoBot. It will use about 350MB of space. This might take awhile to download if you do not have a good internet connection."
-echo ""
-read -n 1 -s -p "Press any key to continue..."
+	echo "This installer will download all of the required packages for NadekoBot. It will use about 350MB of space. This might take awhile to download if you do not have a good internet connection."
+	echo ""
+	read -n 1 -s -p "Press any key to continue..."
 	if [ "$VER" = "14.04" ]; then
-	echo "Gwen was here <3"
-	echo "Preparing..."
-	sudo apt-get install software-properties-common apt-transport-https -y
-	sudo sh -c 'echo "deb [arch=amd64] https://apt-mo.trafficmanager.net/repos/dotnet-release/ trusty main" > /etc/apt/sources.list.d/dotnetdev.list'
-	sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 417A0893
-	sudo add-apt-repository ppa:mc3man/trusty-media -y
-	sudo add-apt-repository ppa:chris-lea/libsodium -y
-	sudo apt update
-	sudo apt upgrade -y
-	sudo apt dist-upgrade -y
-	echo "Installing Git..."
-	sudo apt install git -y
-	echo "Installing .NET Core..."
-	sudo apt install dotnet-dev-1.0.0-preview2.1-003177 -y
-	echo "Installing prerequisites..."
-	sudo apt install libopus0 opus-tools libopus-dev libsodium-dev ffmpeg tmux -y
+		echo "Gwen was here <3"
+		echo "Preparing..."
+		sudo apt install software-properties-common apt-transport-https -y
+		sudo sh -c 'echo "deb [arch=amd64] https://apt-mo.trafficmanager.net/repos/dotnet-release/ trusty main" > /etc/apt/sources.list.d/dotnetdev.list'
+		sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 417A0893
+		sudo add-apt-repository ppa:mc3man/trusty-media -y
+		sudo add-apt-repository ppa:chris-lea/libsodium -y
+		sudo apt update
+		echo "Installing Git..."
+		sudo apt install git -y
+		echo "Installing .NET Core..."
+		sudo apt install dotnet-dev-1.0.0-preview2.1-003177 -y
+		echo "Installing prerequisites..."
+		sudo apt install libopus0 opus-tools libopus-dev libsodium-dev ffmpeg tmux -y
 	elif [ "$VER" = "16.04" ]; then
-	echo ""
-	echo "Preparing..."
-	sudo apt-get install software-properties-common apt-transport-https -y
-	sudo sh -c 'echo "deb [arch=amd64] https://apt-mo.trafficmanager.net/repos/dotnet-release/ xenial main" > /etc/apt/sources.list.d/dotnetdev.list'
-	sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 417A0893
-	sudo apt update
-	sudo apt upgrade -y
-	sudo apt dist-upgrade -y
-	echo "Installing Git..."
-	sudo apt install git -y
-	echo "Installing .NET Core..."
-	sudo apt install dotnet-dev-1.0.0-preview2.1-003177 -y
-	echo "Installing prerequisites..."
-	sudo apt install libopus0 opus-tools libopus-dev libsodium-dev ffmpeg tmux -y
+		echo ""
+		echo "Preparing..."
+		sudo apt install software-properties-common apt-transport-https -y
+		wget -q https://packages.microsoft.com/config/ubuntu/16.04/packages-microsoft-prod.deb -O packages-microsoft-prod.deb
+		sudo dpkg -i packages-microsoft-prod.deb
+		rm packages-microsoft-prod.deb
+		sudo apt update
+		echo "Installing Git..."
+		sudo apt install git -y
+		echo "Installing .NET Core..."
+		sudo apt install dotnet-sdk-2.2 -y
+		echo "Installing prerequisites..."
+		sudo apt install libopus0 opus-tools libopus-dev libsodium-dev ffmpeg tmux -y
 	elif [ "$VER" = "16.10" ]; then
-	echo ""
-	echo "Preparing..."
-	sudo apt-get install software-properties-common apt-transport-https -y
-	sudo sh -c 'echo "deb [arch=amd64] https://apt-mo.trafficmanager.net/repos/dotnet-release/ yakkety main" > /etc/apt/sources.list.d/dotnetdev.list'
-	sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 417A0893
-	sudo apt update
-	sudo apt upgrade -y
-	sudo apt dist-upgrade -y
-	echo "Installing Git..."
-	sudo apt install git -y
-	echo "Installing .NET Core..."
-	sudo apt install dotnet-dev-1.0.0-preview2.1-003177 -y
-	echo "Installing prerequisites..."
-	sudo apt install libopus0 opus-tools libopus-dev libsodium-dev ffmpeg tmux -y
+		echo ""
+		echo "Preparing..."
+		sudo apt install software-properties-common apt-transport-https -y
+		sudo sh -c 'echo "deb [arch=amd64] https://apt-mo.trafficmanager.net/repos/dotnet-release/ yakkety main" > /etc/apt/sources.list.d/dotnetdev.list'
+		sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 417A0893
+		sudo apt update
+		echo "Installing Git..."
+		sudo apt install git -y
+		echo "Installing .NET Core..."
+		sudo apt install dotnet-dev-1.0.0-preview2.1-003177 -y
+		echo "Installing prerequisites..."
+		sudo apt install libopus0 opus-tools libopus-dev libsodium-dev ffmpeg tmux -y
 	elif [ "$VER" = "17.04" ]; then
-	echo ""
-	echo "Preparing..."
-	sudo apt-get install software-properties-common apt-transport-https -y
-	sudo sh -c 'echo "deb [arch=amd64] https://apt-mo.trafficmanager.net/repos/dotnet-release/ yakkety main" > /etc/apt/sources.list.d/dotnetdev.list'
-	sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 417A0893
-	sudo apt update
-	sudo apt upgrade -y
-	sudo apt dist-upgrade -y
-	echo "Installing Git..."
-	sudo apt install git -y
-	echo "Installing .NET Core..."
-	sudo apt install dotnet-dev-1.0.0-preview2.1-003177 -y
-	echo "Installing prerequisites..."
-	sudo apt install libopus0 opus-tools libopus-dev libsodium-dev ffmpeg tmux -y
+		echo ""
+		echo "Preparing..."
+		sudo apt install software-properties-common apt-transport-https -y
+		sudo sh -c 'echo "deb [arch=amd64] https://apt-mo.trafficmanager.net/repos/dotnet-release/ yakkety main" > /etc/apt/sources.list.d/dotnetdev.list'
+		sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 417A0893
+		sudo apt update
+		echo "Installing Git..."
+		sudo apt install git -y
+		echo "Installing .NET Core..."
+		sudo apt install dotnet-dev-1.0.0-preview2.1-003177 -y
+		echo "Installing prerequisites..."
+		sudo apt install libopus0 opus-tools libopus-dev libsodium-dev ffmpeg tmux -y
+	elif [ "$VER" = "18.04" ]; then
+		echo ""
+		echo "Preparing..."
+		sudo apt install software-properties-common apt-transport-https -y
+		wget -q https://packages.microsoft.com/config/ubuntu/18.04/packages-microsoft-prod.deb -O packages-microsoft-prod.deb
+		sudo dpkg -i packages-microsoft-prod.deb
+		rm packages-microsoft-prod.deb
+		sudo add-apt-repository universe
+		sudo apt update
+		echo "Installing Git..."
+		sudo apt install git -y
+		echo "Installing .NET Core..."
+		sudo apt install dotnet-sdk-2.2 -y
+		echo "Installing prerequisites..."
+		sudo apt install libopus0 opus-tools libopus-dev libsodium-dev ffmpeg tmux -y
+	elif [ "$VER" = "19.04" ]; then
+		echo ""
+		echo "Preparing..."
+		sudo apt install software-properties-common apt-transport-https -y
+		wget -q https://packages.microsoft.com/config/ubuntu/19.04/packages-microsoft-prod.deb -O packages-microsoft-prod.deb
+		sudo dpkg -i packages-microsoft-prod.deb
+		rm packages-microsoft-prod.deb
+		sudo apt update
+		echo "Installing Git..."
+		sudo apt install git -y
+		echo "Installing .NET Core..."
+		sudo apt install dotnet-sdk-2.2 -y
+		echo "Installing prerequisites..."
+		sudo apt install libopus0 opus-tools libopus-dev libsodium-dev ffmpeg tmux -y
 	fi
 elif [ "$OS" = "Debian" ]; then
 	if [ "$SVER" = "8" ]; then
 		echo ""
 		echo "Adding .NET to PATH..."
 		apt-get update
-		apt-get upgrade -y
 		apt-get install sudo -y
 		sudo apt-get install software-properties-common apt-transport-https -y
 		sudo apt-get install curl libunwind8 gettext -y
